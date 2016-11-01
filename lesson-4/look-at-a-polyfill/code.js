a316b864c955bd6b7946f87a8b90d153bd0cd699
Richard Kalehoff <richardkalehoff@gmail.com>
2016-11-01 14:29:12 -0700

    add polyfill for String#startsWith

---
+++ lesson-4/look-at-a-polyfill/code.js
@@ -0,0 +1,12 @@
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.substr(position, searchString.length) === searchString;
+    };
+}
+
+
+/* Sample usage */
+'Udacity'.startsWith('Udac'); // returns `true`
+'Udacity'.startsWith('Udac', 2); // returns `false`
+'Udacity'.startsWith('ES6'); // returns `false`
