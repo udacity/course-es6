adcb9ac41be5c16a5095f05ff3ab649bb1d30b24
jamesparkes <parkesrjames@gmail.com>
2016-11-02 14:17:55 -0500

    add hoisting example for let and const

---
+++ lesson-1/let-and-const/code-es5.js
@@ -0,0 +1,10 @@
+function getClothing(isCold) {
+    if (isCold) {
+        var freezing = 'Grab a jacket!';
+    } else {
+        var hot = 'It’s a shorts kind of day.';
+        console.log(freezing);
+    }
+}
+
+getClothing(false);
---
+++ lesson-1/let-and-const/code-es6.js
@@ -0,0 +1,10 @@
+function getClothing(isCold) {
+    if (isCold) {
+        let freezing = 'Grab a jacket!';
+    } else {
+        let hot = 'It’s a shorts kind of day.';
+        console.log(freezing);
+    }
+}
+
+getClothing(false);
